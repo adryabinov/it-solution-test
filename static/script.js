1a6dea0466b56590261d869bc687e71b4e8759e8
andrejrabinov <andryabinov@gmail.com>
2021-04-21 16:51:22 +0300

    add strings nd delay to const

--- static/script.js
+++ static/script.js
@@ -1,6 +1,12 @@
 const message_empty = 'Нет непрочитанных сообщений'
+const message_is_read = 'Прочитано'
+const message_is_not_read = 'Не прочитано'
+
+const get_messages_delay = 10000 // in ms
+
 axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
 axios.defaults.xsrfCookieName = "csrftoken";
+
 
 Vue.component('message', {
   delimiters: ["[[", "]]"],
@@ -15,10 +21,19 @@ Vue.component('message', {
 
   computed: {
     status: function () {
-        return this.is_read ? 'true' : 'false'
+        return this.is_read ? message_is_read : message_is_not_read
         }
     },
-  template: '<div class="card-body"><h3 class="card-title">[[ id ]]</h3><h2 class="card-subtitle mb-2 text-muted">[[ status ]]</h2><p class="card-text">[[ text ]]</p><div class="d-grid gap-2 col-6 mx-auto"><button v-on:click="markRead([[ id ]])" type="button" class="btn btn-primary">mark read</button></div></div>'
+  template: `
+    <div class="card-body">
+        <h3 class="card-title">[[ id ]]</h3>
+        <h2 class="card-subtitle mb-2 text-muted">[[ status ]]</h2>
+        <p class="card-text">[[ text ]]</p>
+        <div class="d-grid gap-2 col-6 mx-auto">
+            <button v-on:click="markRead([[ id ]])" type="button" class="btn btn-primary">прочитано</button>
+        </div>
+    </div>
+  `,
 })
 
 var app = new Vue({
@@ -48,6 +63,6 @@ var app = new Vue({
         this.updateMessages()
         this.interval = setInterval(() => {
             this.updateMessages()
-        },10000)
+        }, get_messages_delay)
   },
-  })
+})
