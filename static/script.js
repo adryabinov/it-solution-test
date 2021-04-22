d454ff67e9dc986fae6a69287d1a05c861670f5f
andrejrabinov <andryabinov@gmail.com>
2021-04-22 10:32:43 +0300

    add strings to consts and dynamic classes

--- static/script.js
+++ static/script.js
@@ -1,6 +1,7 @@
 const message_empty = 'Нет непрочитанных сообщений'
 const message_is_read = 'Прочитано'
 const message_is_not_read = 'Не прочитано'
+const btn_read_text = 'прочитано'
 
 const get_messages_delay = 10000 // in ms
 
@@ -11,7 +12,11 @@ axios.defaults.xsrfCookieName = "csrftoken";
 Vue.component('message', {
   delimiters: ["[[", "]]"],
   props: ['index','id','text', 'is_read'],
-
+  data() {
+    return {
+        'btn_read_text': btn_read_text,
+    }
+  },
   methods: {
     markRead: async function (id) {
         await axios.post('api/mark_read?id=' + id)
@@ -25,14 +30,24 @@ Vue.component('message', {
         }
     },
   template: `
+  <div class='card'>
+    <div class="card-header">
+        <h3 class="card-title h5">#[[ id ]]</h3>
+        <h2 class="mb-2h6 badge" :class="this.is_read ? 'bg-success' : 'bg-secondary'">
+            [[ status ]]
+        </h2>
+    </div>
     <div class="card-body">
-        <h3 class="card-title">[[ id ]]</h3>
-        <h2 class="card-subtitle mb-2 text-muted">[[ status ]]</h2>
         <p class="card-text">[[ text ]]</p>
         <div class="d-grid gap-2 col-6 mx-auto">
-            <button v-on:click="markRead([[ id ]])" type="button" class="btn btn-primary">прочитано</button>
+            <button v-on:click="markRead([[ id ]])" type="button" class="btn"
+                    :class="this.is_read ? 'btn-outline-secondary' : 'btn-primary'"
+                    :disabled="this.is_read ? '' : disabled">
+                [[ btn_read_text ]]
+            </button>
         </div>
     </div>
+  </div>
   `,
 })
 
