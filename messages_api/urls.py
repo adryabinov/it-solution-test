b6a7114115d425b40b658112b66599df51d8ba28
andrejrabinov <andryabinov@gmail.com>
2021-04-22 10:33:13 +0300

    add class based views

--- messages_api/urls.py
+++ messages_api/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('api/get_messages', views.unread_json, name='get_messages'),
-    path('api/mark_read', views.mk_read, name='mark_read'),
+    path('api/get_messages', views.GetMessages.as_view(), name='get_messages'),
+    path('api/mark_read', views.MarkRead.as_view(), name='mark_read'),
 ]
--- messages_api/views.py
+++ messages_api/views.py
@@ -1,26 +1,34 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from django.http import JsonResponse
 from django.views.generic import TemplateView
+from django.views import View
 from messages_api.models import MessageFromSpace
-
-# Create your views here.
 
 
 class IndexView(TemplateView):
     template_name = "index.html"
 
+    def get_context_data(self, **kwargs):
+        context = {
+            'title': 'сообщения из космоса',
+            'page_title': 'СООБЩЕНИЯ ИЗ КОСМОСА'
+        }
+        return context
 
-def unread_json(request):
-    messages = [MessageFromSpace.serialize(message) for message
-                in MessageFromSpace.objects.filter(is_read=False).order_by('-pk')]
-    return JsonResponse(messages, safe=False)
+
+class GetMessages(View):
+    def get(self, request):
+        messages = [MessageFromSpace.serialize(message) for message
+                    in
+                    MessageFromSpace.objects.filter(is_read=False).order_by(
+                        '-pk')]
+        return JsonResponse(messages, safe=False)
 
 
-def mk_read(request):
-    read_id = request.GET.get('id')
-    message = MessageFromSpace.objects.get(pk=read_id)
-    message.is_read = True
-    message.save()
+class MarkRead(View):
+    def post(self, request):
+        read_id = request.GET.get('id')
+        message = MessageFromSpace.objects.get(pk=read_id)
+        message.is_read = True
+        message.save()
 
-    return JsonResponse(MessageFromSpace.serialize(message), safe=False)
+        return JsonResponse(MessageFromSpace.serialize(message), safe=False)
